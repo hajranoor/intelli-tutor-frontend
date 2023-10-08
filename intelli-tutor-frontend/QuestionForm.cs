a6b50269eb180bfd3b57cccd908e7bb3afccf584
hajranoor <92498510+hajranoor@users.noreply.github.com>
2023-10-08 17:53:23 +0500

    question-form-updated

--- intelli-tutor-frontend/QuestionForm.cs
+++ intelli-tutor-frontend/QuestionForm.cs
@@ -12,6 +12,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace intelli_tutor_frontend
 {
@@ -27,6 +29,31 @@ namespace intelli_tutor_frontend
 
         string path;
 
+        
+
+        
+
+
+        string json = @"
+        [
+            {
+                ""input_value1"": [""int m =1;"",""int n =2;""],
+                ""input_value2"": ""World"",
+                ""expected_output"": 3
+            },
+            {
+                ""input_value1"": [""int m =3;"",""int n =4;""],
+                ""input_value2"": 7,
+                ""expected_output"": 7
+            },
+            {
+                ""input_value1"": [""int m =5;"",""int n =6;""],
+                ""input_value2"": [4, 5, 6],
+                ""expected_output"": 5
+            }
+        ]";
+
+
         private async void QuestionForm_Load(object sender, EventArgs e)
         {
             //TestCasesApi testCasesApi = new TestCasesApi();
@@ -36,18 +63,25 @@ namespace intelli_tutor_frontend
             //int count = 1;
             //foreach (var item in testcaseList)
             //{
-                //questionBox.Text += "Test Case " + count + " : ";
-                //count++;
-                //questionBox.Text += "Input : ";
-                //foreach (var item1 in item.input_data)
+            //questionBox.Text += "Test Case " + count + " : ";
+            //count++;
+            //questionBox.Text += "Input : ";
+            //foreach (var item1 in item.input_data)
 
-                //{
-                    //questionBox.Text += item1;
-                //}
-                //questionBox.Text += "Output : " + item.output_data;
+            //{
+            //questionBox.Text += item1;
+            //}
+            //questionBox.Text += "Output : " + item.output_data;
             //}
 
-                //MessageBox.Show(problem.description);
+            //MessageBox.Show(problem.description);
+
+
+
+            
+
+
+
             Console.WriteLine("this is path");
 
             string applicationDirectory = AppDomain.CurrentDomain.BaseDirectory;
@@ -63,6 +97,9 @@ namespace intelli_tutor_frontend
                 string displayName = Path.GetFileName(compiler);
                 compilerComboBox1.Items.Add(displayName);
             }
+            //codeBox.Text = "#include <iostream>\r\n\r\nint main() {\r\n\r\n}\r\n";  //from backend
+
+
 
 
         }
@@ -83,7 +120,7 @@ namespace intelli_tutor_frontend
                 javaClass javaClassObj = new javaClass(path, codeText);
                 //string returnValue = javaClassObj.CompileCode(codeText);
                 string startercode = "public class java {\r\n    public static void main(String[] args) {\r\n        int n = 5;\r\n    }\r\n}\r\n";
-                string trigger  = @"^\s*int\s+n\s*=\s*5\s*;";
+                string trigger  = @"^\s*int\s+main\s*\(\s*\)\s*{[^{}]*\s*}";
                 string returnValue2 = javaClassObj.compileType1(startercode, codeText, trigger);
 
 
@@ -93,8 +130,14 @@ namespace intelli_tutor_frontend
 
             if (compilerComboBox1.Text == "c++")
             {
+                JArray jsonArray = JArray.Parse(json);  //from backend
+                string removingPattern = @"int\s+n\s*=\s*\d+\s*;|int\s+m\s*=\s*\d+\s*;";  //from backend
+                string startercode = "#include <iostream>\r\n\r\nint main() {\r\n\r\n}\r\n";  //from backend
+                string trigger = @"^\s*int\s+main\s*\(\s*\)\s*{";   //from backend
                 string codeText = codeBox.Text;
-                cppClass cppClassObj = new cppClass();
+                cppClass cppClassObj = new cppClass(path, codeText);
+                //cppClassObj.CompileCode(codeText);
+                cppClassObj.compileType1(startercode, codeText, trigger, jsonArray, removingPattern);
             }
 
             if (compilerComboBox1.Text == "c#")
---
+++ intelli-tutor-frontend/bin/Debug/files/cplus.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+int n =6;
+int m =5;
+
+
+
+
+
+        std::cout << m + n<< std::endl;
+
+}
